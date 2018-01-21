810563dff8fa1c0f945125cb34981a287d3c033f
Ruslan <maestroprog@gmail.com>
2018-01-21 14:54:01 +0300

    Added minimalistic autowire feature. (#3)
    * Added minimalistic autowire feature.
    
    * Some improvements for autowire.

--- sample/index.php
+++ sample/index.php
@@ -3,10 +3,8 @@ ini_set('display_errors', true);
 error_reporting(E_ALL);
 
 use Maestroprog\Container\Container;
-use Maestroprog\Container\ContainerCompiler;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
 require_once __DIR__ . '/CacheInterface.php';
 require_once __DIR__ . '/FileCache.php';
 require_once __DIR__ . '/InMemoryCache.php';
@@ -14,19 +12,22 @@ require_once __DIR__ . '/CacheDecorator.php';
 require_once __DIR__ . '/SampleContainer.php';
 require_once __DIR__ . '/SampleOverrideContainer.php';
 require_once __DIR__ . '/SampleService.php';
+require_once __DIR__ . '/SampleServiceUsingFileCache.php';
 
 $container = Container::instance();
 $container->register(new SampleOverrideContainer());
 $container->register(new SampleContainer());
 
-$compiler = new ContainerCompiler($container);
-$compiler->compile(__DIR__ . '/container.php');
+var_dump($container->get(\TestNamespace\SampleServiceUsingFileCache::class));
 
-require_once __DIR__ . '/container.php';
-$container = new CompiledContainer($container);
+//$compiler = new ContainerCompiler($container);
+//$compiler->compile(__DIR__ . '/container.php');
 
-/** @var SampleService $service */
-$service = $container->get(SampleService::class);
+//require_once __DIR__ . '/container.php';
+//$container = new CompiledContainer($container);
+
+$service = $container->get(\TestNamespace\SampleService::class);
 
 var_dump($service->getSample1());
 var_dump($container->getSampleService()->getSample1());
+var_dump($container->get('SampleServiceAutoWire'));
